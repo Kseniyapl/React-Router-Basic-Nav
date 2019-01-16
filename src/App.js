ea6680f6e4942fbe1051c80ab8e6c6f4773ea32c
Kseniya Platonava <ksu2008@gmail.com>
2019-01-16 00:22:48 -0500

    Do Basic Navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { Route } from 'react-router-dom'; 
+import Navigaton from './components/Navigation'
+
 
 const App = () => (
-  <div>
-    <Navigation />
+  <div className="App">
+   <Navigation />
+    <Route exact path="/" component={Home} />
+    <Route path="/about" component={About} />
+    <Route path="/contact" component={Contact} />
+
   </div>
 );
 
